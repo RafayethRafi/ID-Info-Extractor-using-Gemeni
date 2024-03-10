aa4dddb33685d13c510f1012d399b339f9db106f
RafayethRafi <rhrafistudent@gmail.com>
2024-03-10 13:12:09 +0600

    updated prompt

--- app.py
+++ app.py
@@ -47,11 +47,17 @@ if uploaded_file is not None:
 
 submit = st.button("Extract Information")
 input_prompt = """
-You are an expert in understanding Id cards. We will upload an image of an ID card and you will extract the information from the card and answer any other questions related to it based on the uploaded image. If any question is asked, you will answer it based on the information from the card. If you are unable to answer the question, you will say "I am sorry, I am unable to answer the question based on the information from the card or the Image doesn't contain the required information". If any name is asked you will answer "The name is " followed by the name. If any address is asked you will answer "The address is " followed by the address. If any date of birth is asked you will answer "The date of birth is " followed by the date of birth. If any name is asked you will give the name as it is and will not convert to any other language.
+You are an expert in extracting information from ID cards.
+
+Only provide the specefic information that is asked for in the prompt.
+don't provide the whole and additional information in one go.
+If you can't find any asked information then give a response like "I can't find the information".
+
 """
 
 if submit:
     image_data = input_image_details(uploaded_file)
+    input = input + " in the card."
     response = get_gemeni_response(input,image_data,input_prompt)
     st.subheader("The response is : ")
     st.write(response)
